029b8c27965ad5999ea3e348a9c3d51114a2b6ea
qsdcv301 <qsdcv301@naver.com>
2024-11-06 00:32:48 +0900

    setting:EzenBookStore 초기설정

---
+++ src/main/java/ezen/team/ezenbookstore/EzenBookStoreApplication.java
@@ -0,0 +1,13 @@
+package ezen.team.ezenbookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzenBookStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EzenBookStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ezen/team/ezenbookstore/EzenBookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package ezen.team.ezenbookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzenBookStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
