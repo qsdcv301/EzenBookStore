6d866a40888ccfc187027b33641a7688269aba6e
qsdcv301 <130839376+qsdcv301@users.noreply.github.com>
2024-11-08 10:11:12 +0900

    feat:Book 검색 시스템을 위한 Repository, Service 추가
    국내
     카테고리
      세부 카테고리
    국외
     카테고리
      세부 카테고리
    로 구성하되 세부 카테고리를 선택하여 검색할 수 있도록 기획한 내용에 따른 Service Repository

--- src/main/java/ezen/team/ezenbookstore/repository/BookRepository.java
+++ src/main/java/ezen/team/ezenbookstore/repository/BookRepository.java
@@ -4,7 +4,33 @@ import ezen.team.ezenbookstore.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
+
+    List<Book> findByTitleContaining(String title);
+
+    List<Book> findByAuthorContaining(String author);
+
+    List<Book> findByPublisherContaining(String publisher);
+
+    List<Book> findByIsbnContaining(Integer isbn);
+
+//    List<Book> findByTitleContainingAndCategoryIdAndSubcategoryId(String title, Integer categoryId, Integer subcategoryId);
+//
+//    List<Book> findByAuthorContainingAndCategoryIdAndSubcategoryId(String author, Integer categoryId, Integer subcategoryId);
+//
+//    List<Book> findByPublisherContainingAndCategoryIdAndSubcategoryId(String publisher, Integer categoryId, Integer subcategoryId);
+//
+//    List<Book> findByIsbnContainingAndCategoryIdAndSubcategoryId(String isbn, Integer categoryId, Integer subcategoryId);
+
+    List<Book> findByTitleContainingAndCategoryIdAndSubcategoryIdAndIfkr(String title, Integer categoryId, Integer subcategoryId, Byte ifkr);
+
+    List<Book> findByAuthorContainingAndCategoryIdAndSubcategoryIdAndIfkr(String author, Integer categoryId, Integer subcategoryId, Byte ifkr);
+
+    List<Book> findByPublisherContainingAndCategoryIdAndSubcategoryIdAndIfkr(String publisher, Integer categoryId, Integer subcategoryId, Byte ifkr);
+
+    List<Book> findByIsbnContainingAndCategoryIdAndSubcategoryIdAndIfkr(String isbn, Integer categoryId, Integer subcategoryId, Byte ifkr);
 
 }
