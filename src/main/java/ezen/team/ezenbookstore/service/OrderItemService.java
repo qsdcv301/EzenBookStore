4033659174ba7d1b7c2534636bad101f55815cb4
qsdcv301 <130839376+qsdcv301@users.noreply.github.com>
2024-11-29 12:08:25 +0900

    fix:OrderController, OrderItemService, OrderService
    OrderController에서 불필요한 역할을 OrderItemService와 OrderService로 이관

--- src/main/java/ezen/team/ezenbookstore/service/OrderItemService.java
+++ src/main/java/ezen/team/ezenbookstore/service/OrderItemService.java
@@ -1,28 +1,24 @@
 package ezen.team.ezenbookstore.service;
 
 import ezen.team.ezenbookstore.entity.OrderItem;
+import ezen.team.ezenbookstore.entity.User;
 import ezen.team.ezenbookstore.repository.OrderItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class OrderItemService {
 
     private final OrderItemRepository orderItemRepository;
+    private final UserService userService;
 
     public OrderItem findById(Long id) {
         return orderItemRepository.findById(id).orElse(null);
-    }
-
-    public List<OrderItem> findAll() {
-        return orderItemRepository.findAll();
-    }
-
-    public OrderItem create(OrderItem orderItem) {
-        return orderItemRepository.save(orderItem);
     }
 
     public OrderItem update(OrderItem orderItem) {
@@ -36,7 +32,60 @@ public class OrderItemService {
         return orderItemRepository.save(newOrderItem);
     }
 
-    public void delete(Long id) {
-        orderItemRepository.deleteById(id);
+    public Map<String, String> buildOrderItemResponse(String orderItemId, @ModelAttribute("user") User user) {
+        Map<String, String> response = new HashMap<>();
+        Long id = Long.parseLong(orderItemId);
+        OrderItem orderItem = findById(id);
+        String title = orderItem.getBook().getTitle();
+        String author = orderItem.getBook().getAuthor();
+        String publisher = orderItem.getBook().getPublisher();
+        String price = orderItem.getBook().getPrice().toString();
+        String stock = orderItem.getQuantity().toString();
+        String status = orderItem.getStatus().toString();
+        String bookId = orderItem.getBook().getId().toString();
+        String userGrade = user.getGrade().toString();
+        response.put("success", "true");
+        response.put("bookId", bookId);
+        response.put("orderItemId", id.toString());
+        response.put("orderItemTitle", title);
+        response.put("orderItemAuthor", author);
+        response.put("orderItemPublisher", publisher);
+        response.put("orderItemPrice", price);
+        response.put("orderItemStock", stock);
+        response.put("userGrade", userGrade);
+        response.put("orderItemStatus", status);
+        return response;
     }
+
+    public Map<String, String> updateOrderItemAndUserPoint(Long orderItemId, Long point, @ModelAttribute("user") User user) {
+        Map<String, String> response = new HashMap<>();
+        User newUser = User.builder()
+                .id(user.getId())
+                .provider(user.getProvider())
+                .email(user.getEmail())
+                .name(user.getName())
+                .password(user.getPassword())
+                .tel(user.getTel())
+                .addr(user.getAddr())
+                .addrextra(user.getAddrextra())
+                .createdAt(user.getCreatedAt())
+                .birthday(user.getBirthday())
+                .grade(user.getGrade())
+                .point(user.getPoint() + point)
+                .build();
+        userService.update(newUser);
+        OrderItem orderItem = findById(orderItemId);
+        byte status = 3;
+        OrderItem newOrderItem = OrderItem.builder()
+                .id(orderItem.getId())
+                .book(orderItem.getBook())
+                .orders(orderItem.getOrders())
+                .quantity(orderItem.getQuantity())
+                .status(status)
+                .build();
+        update(newOrderItem);
+        response.put("success", "true");
+        return response;
+    }
+
 }
--- src/main/java/ezen/team/ezenbookstore/service/OrdersService.java
+++ src/main/java/ezen/team/ezenbookstore/service/OrdersService.java
@@ -1,31 +1,23 @@
 package ezen.team.ezenbookstore.service;
 
-import ezen.team.ezenbookstore.entity.Orders;
+import ezen.team.ezenbookstore.entity.*;
 import ezen.team.ezenbookstore.repository.OrdersRepository;
+import ezen.team.ezenbookstore.util.FormatUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.sql.Timestamp;
-import java.time.LocalDate;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
 public class OrdersService {
 
     private final OrdersRepository orderRepository;
+    private final FileUploadService fileUploadService;
 
     public Orders findById(Long id) {
         return orderRepository.findById(id).orElse(null);
-    }
-
-    public List<Orders> findAll() {
-        return orderRepository.findAll();
-    }
-
-    public Orders create(Orders order) {
-        return orderRepository.save(order);
     }
 
     public Orders update(Orders order) {
@@ -40,85 +32,102 @@ public class OrdersService {
         return orderRepository.save(newOrder);
     }
 
-    public void deleteById(Long id) {
-        orderRepository.deleteById(id);
+    public void cancelOrder(Long orderId) {
+        Orders orders = findById(orderId);
+        Orders newOrders = Orders.builder()
+                .id(orders.getId())
+                .user(orders.getUser())
+                .delivery(orders.getDelivery())
+                .payment(orders.getPayment())
+                .orderDate(orders.getOrderDate())
+                .status((byte) 3)
+                .orderItems(orders.getOrderItems())
+                .build();
+        update(newOrders);
     }
 
-    public List<Orders> findAllByUserId(Long userId) {
-        return orderRepository.findAllByUserId(userId);
-    }
+    public Map<String, Object> buildOrderResponse(Orders order) {
+        Map<String, Object> response = new HashMap<>();
 
-    public List<Orders> findByUserEmail(String email) {
-        return orderRepository.findByUserEmail(email);
-    }
+        List<String> titleList = new ArrayList<>();
+        List<String> authorList = new ArrayList<>();
+        List<String> publisherList = new ArrayList<>();
+        List<String> priceList = new ArrayList<>();
+        List<String> stockList = new ArrayList<>();
+        List<String> imageList = new ArrayList<>();
+        List<String> orderItemList = new ArrayList<>();
+        List<String> orderItemListStatus = new ArrayList<>();
 
-    public List<Orders> findByOrderId(Long id) {
-        return orderRepository.findById(id).map(List::of).orElse(Collections.emptyList());
-    }
-
-    public List<Orders> findAllByOrderDateBetweenAndUserId(LocalDate startDate, LocalDate endDate, Long userId) {
-        Timestamp startTimestamp = Timestamp.valueOf(startDate.atStartOfDay());
-        Timestamp endTimestamp = Timestamp.valueOf(endDate.atStartOfDay().plusDays(1).minusNanos(1));
-        return orderRepository.findAllByOrderDateBetweenAndUserId(startTimestamp, endTimestamp, userId);
-    }
-
-    public List<Orders> findAllByDelivery_StatusAndUserId(Byte deliveryStatus, Long userId) {
-        return orderRepository.findAllByDelivery_StatusAndUserId(deliveryStatus, userId);
-    }
-
-    public List<Orders> findAllByStatusAndUserId(Byte orderStatus, Long userId) {
-        return orderRepository.findAllByStatusAndUserId(orderStatus, userId);
-    }
-
-    public List<Orders> findAllByOrderItemsBookTitleContainingAndUserId(String keyword, Long userId) {
-        return orderRepository.findAllByOrderItems_Book_TitleContainingAndUserId(keyword, userId);
-    }
-
-    public List<Orders> filterOrders(LocalDate startDate, LocalDate endDate, Byte deliveryStatus, Byte orderStatus, String keyword, Long userId) {
-        List<Orders> filteredOrders = findAllByUserId(userId);
-        boolean isFirstFilter = true;
-
-        if (startDate != null && endDate != null) {
-            List<Orders> ordersByDateRange = findAllByOrderDateBetweenAndUserId(startDate, endDate, userId);
-            if (isFirstFilter) {
-                filteredOrders = ordersByDateRange;
-                isFirstFilter = false;
-            } else {
-                filteredOrders.retainAll(ordersByDateRange);
+        for (OrderItem orderItem : order.getOrderItems()) {
+            titleList.add(orderItem.getBook().getTitle());
+            authorList.add(orderItem.getBook().getAuthor());
+            publisherList.add(orderItem.getBook().getPublisher());
+            priceList.add(orderItem.getBook().getPrice().toString());
+            stockList.add(orderItem.getBook().getStock().toString());
+            orderItemList.add(orderItem.getId().toString());
+            orderItemListStatus.add(orderItem.getStatus().toString());
+            // 이미지 파일 경로 찾기
+            String imagePath = fileUploadService.findImageFilePath(orderItem.getBook().getId(), "book");
+            if (imagePath != null) {
+                imageList.add(imagePath);
             }
         }
 
-        if (deliveryStatus != null) {
-            List<Orders> ordersByDeliveryStatus = findAllByDelivery_StatusAndUserId(deliveryStatus, userId);
-            if (isFirstFilter) {
-                filteredOrders = ordersByDeliveryStatus;
-                isFirstFilter = false;
-            } else {
-                filteredOrders.retainAll(ordersByDeliveryStatus);
-            }
-        }
+        response.put("titleList", titleList);
+        response.put("authorList", authorList);
+        response.put("publisherList", publisherList);
+        response.put("priceList", priceList);
+        response.put("stockList", stockList);
+        response.put("imageList", imageList);
+        response.put("orderItemList", orderItemList);
+        response.put("orderItemListStatus", orderItemListStatus);
 
-        if (orderStatus != null) {
-            List<Orders> ordersByOrderStatus = findAllByStatusAndUserId(orderStatus, userId);
-            if (isFirstFilter) {
-                filteredOrders = ordersByOrderStatus;
-                isFirstFilter = false;
-            } else {
-                filteredOrders.retainAll(ordersByOrderStatus);
-            }
-        }
+        String orderStatus = order.getStatus().toString();
+        Timestamp orderDate = order.getOrderDate();
+        String formattedDate = FormatUtils.formatDate(orderDate);
+        String deliveryTracking = order.getDelivery().getTrackingNum();
+        String deliveryAddr = order.getDelivery().getAddr();
+        String deliveryAddrextra = order.getDelivery().getAddrextra();
+        String deliveryName = order.getDelivery().getName();
+        String deliveryTel = order.getDelivery().getTel();
+        String formattedPaymentAmount = FormatUtils.formatCurrency(order.getPayment().getAmount());
 
-        if (keyword != null && !keyword.isEmpty()) {
-            List<Orders> ordersByKeyword = findAllByOrderItemsBookTitleContainingAndUserId(keyword, userId);
-            if (isFirstFilter) {
-                filteredOrders = ordersByKeyword;
-                isFirstFilter = false;
-            } else {
-                filteredOrders.retainAll(ordersByKeyword);
-            }
-        }
+        String status = switch (order.getStatus()) {
+            case 1 -> "주문 완료";
+            case 2 -> "주문 취소";
+            case 3 -> "교환/반품 신청";
+            case 4 -> "교환/반품 승인";
+            default -> "기타";
+        };
+        String deliveryStatus = switch (order.getDelivery().getStatus()) {
+            case 1 -> "배송 준비중";
+            case 2 -> "배송중";
+            case 3 -> "배송 완료";
+            case 4 -> "반송 준비중";
+            case 5 -> "반송중";
+            case 6 -> "반송 완료";
+            default -> "기타";
+        };
+        String paymentStatus = switch (order.getPayment().getStatus()) {
+            case 1 -> "결제 완료";
+            case 2 -> "결제 취소";
+            case 3 -> "재결제 완료";
+            default -> "기타";
+        };
 
-        return filteredOrders;
+        response.put("orderStatus", orderStatus);
+        response.put("orderDate", formattedDate);
+        response.put("deliveryTracking", deliveryTracking);
+        response.put("deliveryAddr", deliveryAddr);
+        response.put("deliveryAddrextra", deliveryAddrextra);
+        response.put("deliveryName", deliveryName);
+        response.put("deliveryTel", deliveryTel);
+        response.put("paymentAmount", formattedPaymentAmount);
+        response.put("paymentStatus", paymentStatus);
+        response.put("status", status);
+        response.put("deliveryStatus", deliveryStatus);
+        response.put("success", "true");
+
+        return response;
     }
-
 }
