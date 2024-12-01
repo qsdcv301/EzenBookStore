0d3157c4406741532984845e760d8ded532faefa
qsdcv301 <qsdcv301@naver.com>
2024-12-02 00:46:07 +0900

    feat:javadoc 명령어의 활용
    /** */ 형식으로 사용
    * 일반 설명
    * @param 파라미터에 설명
    * @return 반환값에 대한 설명
    * @exception 발생하는 예외에 대한 설명
    * @see 참고 주제, 관련 주제

--- src/main/java/ezen/team/ezenbookstore/service/FileUploadService.java
+++ src/main/java/ezen/team/ezenbookstore/service/FileUploadService.java
@@ -15,6 +15,16 @@ public class FileUploadService {
     @Value("${file.upload-dir}")
     private String uploadDir;
 
+    /**
+     * 파일 업로드 메서드
+     * create / update 활용 가능, 기존 파일이 있다면 삭제 후 업로드
+     * @param file 업르할 이미지 파일
+     * @param id 생성된 타입의 id값
+     * @param type 업로드 될 파일의 타입 예) 책=book, 리뷰=review, 공지사항=notice, 이벤트=event
+     * @return boolean 타입으로 생성되었는지 안되었는지 확인 가능
+     * @exception IOException 입출력에 예외 발생
+     * @see ezen.team.ezenbookstore.service.FileUploadService#findImageFilePath(Long, String)
+     */
     public boolean uploadFile(MultipartFile file, String id, String type) {
         // 선택된 파일이 있는지 확인
         if (file.isEmpty()) {
